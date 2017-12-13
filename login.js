cc6354481b135ca6e82722b09366161d8f28bc94
Milos <milos.isailovic@htecgroup.com>
2017-12-13 14:02:37 +0100

    third commit

--- login.js
+++ login.js
@@ -12,3 +12,31 @@ cy.get('#password').type('Ggaporousp6!');
 }
 
 login();
+
+
+// loginUsr: function(uname, pass){
+//         let that = this;
+//         this.elements._loginButton().click().then(function () {
+//             browser.getAllWindowHandles().then(function (handles) {
+//                 let oldWindowHandle = handles[0];
+//                 let newWindowHandle = handles[1]; // this is your new window
+//
+//                 browser.switchTo().window(newWindowHandle).then(function () {
+//                     browser.ignoreSynchronization = true;
+//                     global.waitEle(that.elements._gptwButton());
+//                     that.elements._gptwButton().click();
+//                     that.elements._oldView().click();
+//                     global.waitEle(that.elements._usernameField());
+//                     that.elements._usernameField().sendKeys(uname);
+//                     that.elements._passwordField().click();
+//                     global.waitEle(that.elements._passwordInput());
+//                     that.elements._passwordInput().sendKeys(pass);
+//                     global.waitEle(that.elements._submitButton());
+//                     that.elements._submitButton().click();
+//                 });
+//                 browser.switchTo().window(oldWindowHandle);
+//                 //global.waitEle(home.elements._standardLibrary());
+//                 //expect(browser.getTitle()).toEqual('GPTW');
+//             });
+//         });
+//     }
